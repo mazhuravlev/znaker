c62e025314ba6462b1725727f237a3e59c8ad983
Mikhail Zhuravlev <zhuravlev@etton.ru>
2016-08-26 14:25:48 +0300

    sitemap_1

--- src/Znaker/Controllers/SitemapController.cs
+++ src/Znaker/Controllers/SitemapController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 using PostgreSqlProvider;
 using Znaker.Models;
@@ -10,6 +11,9 @@ namespace Znaker.Controllers
     [Route("sitemap")]
     public class SitemapController : Controller
     {
+        private const int ContactsBySitemap = 50000;
+        private const string SitemapDateFormat = "yyyy-MM-ddTHH:mm:sszzz";
+
         private readonly PostgreSqlContext _db;
 
         public SitemapController(PostgreSqlContext db)
@@ -20,15 +24,18 @@ namespace Znaker.Controllers
         [Route("index.xml")]
         public IActionResult Index()
         {
-            // TODO: сделать настоящий индекс
-            var totalMaps = Math.Ceiling(_db.Contacts.Count() / 50000D);
+            var totalMaps = Math.Ceiling(_db.Contacts.Count() / (decimal) ContactsBySitemap);
             var model = new List<SitemapIndexModel>();
             for (var i = 0; i < totalMaps; i++)
             {
                 model.Add(new SitemapIndexModel
                 {
-                    Loc = $"sitemap_{i + 1}.xml",
-                    Lastmod = _db.Contacts.Skip(50000 * i).Take(50000).Max(c => c.UpdatedOn).ToString("yyyy-MM-ddTHH:mm:sszzz")
+                    Loc = $"https://znaker.ru/sitemap_{i + 1}.xml",
+                    Lastmod =
+                        _db.Contacts.Skip(ContactsBySitemap * i)
+                            .Take(ContactsBySitemap)
+                            .Max(c => c.UpdatedOn)
+                            .ToString(SitemapDateFormat)
                 });
             }
             Response.ContentType = "application/xml";
@@ -38,8 +45,16 @@ namespace Znaker.Controllers
         [Route("sitemap_{id}.xml")]
         public IActionResult Sitemap(int id)
         {
+            var urls = _db.Contacts.Skip(ContactsBySitemap * (id - 1))
+                .Take(ContactsBySitemap)
+                .Select(c => new SitemapModel
+                {
+                    Loc = "https://znaker.ru/" + HtmlEncoder.Default.Encode(c.Identity),
+                    Lastmod = c.UpdatedOn.ToString(SitemapDateFormat)
+                })
+                .ToList();
             Response.ContentType = "application/xml";
-            return View();
+            return View(urls);
         }
     }
 }
---
+++ src/Znaker/Models/SitemapModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Znaker.Models
+{
+    public class SitemapModel
+    {
+        public string Loc;
+        public string Lastmod;
+        public string ChangeFreq = "weekly";
+        public double Priority = 0.8;
+    }
+}
