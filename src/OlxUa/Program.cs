715b5ba433daf39260cc3b21f002906c10d0f849
Mikhail Zhuravlev <zhuravlev@etton.ru>
2016-08-29 09:52:36 +0300

    olx codes test

---
+++ src/OlxUa/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OlxUa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string urlTemplate = "https://ssl.olx.ua/i2/ajax/ad/getcontact/?type=phone&json=1&id={0}&version=2.3.2";
+            var startId = 148486659;
+            var offset = 0;
+            var okCount = 0;
+            var notFountCount = 0;
+            while (true)
+            {
+                var result = GetCode(string.Format(urlTemplate, startId + offset)).Result;
+                switch (result)
+                {
+                    case HttpStatusCode.NotFound:
+                        notFountCount++;
+                        break;
+                    case HttpStatusCode.OK:
+                        okCount++;
+                        break;
+                    default:
+                        Console.WriteLine(result.ToString());
+                        break;
+                }
+                offset--;
+                Console.WriteLine(string.Format("OK: {0}, NotFound: {1}", okCount, notFountCount));
+            }
+
+        }
+
+        public static async Task<HttpStatusCode> GetCode(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            return response.StatusCode;
+        }
+    }
+}
