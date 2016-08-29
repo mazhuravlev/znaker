f994d513f04a2158697028557c5862b93ca3f01d
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-08-29 23:09:21 +0300

    lx sitemap

--- src/OlxUa/Program.cs
+++ src/OlxUa/Program.cs
@@ -1,58 +1,83 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace OlxUa
+namespace ConsoleApp1
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            //var xdoc = XDocument.Load("map.xml");
+            var sitemap = downloadSitemap(0).Result;
+            var xdoc = XDocument.Parse(sitemap);
+            var adIds = GetSitemapIds(xdoc);
+            Console.ReadKey();
         }
 
-        public static async Task<HttpStatusCode> GetCode(string url)
+        static async Task<string> downloadSitemap(int id)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(url);
-            return response.StatusCode;
+            var client = new System.Net.Http.HttpClient();
+            var sitemapUrlTemplate = "http://olx.ua/sitemap-ads-{0}.xml";
+            return await client.GetStringAsync(string.Format(sitemapUrlTemplate, id));
         }
 
-        public static void IncrementSearch()
+        static List<long> GetSitemapIds(XDocument xdoc)
         {
-            const string urlTemplate =
-                "https://ssl.olx.ua/i2/ajax/ad/getcontact/?type=phone&json=1&id={0}&version=2.3.2";
-            var startId = 148486659;
-            var offset = 0;
-            var okCount = 0;
-            var notFountCount = 0;
-            while (true)
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            var regex = new Regex(@"ID([a-zA-Z0-9]+)");
+            return xdoc.Root.Elements(ns + "url")
+                .Select(u => u.Element(ns + "loc").Value)
+                .Select(s => regex.Match(s).Groups[1].Value)
+                .Select(s => DecryptOlxId(s))
+                .ToList();
+        }
+
+        static long DecryptOlxId(string olxId)
+        {
+            return ArbitraryToDecimalSystem(SwapCase(olxId), 62);
+        }
+
+        static string SwapCase(string input)
+        {
+            return new string(input.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ?
+                       char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+        }
+
+        public static long ArbitraryToDecimalSystem(string number, int radix)
+        {
+            const string Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (radix < 2 || radix > Digits.Length)
             {
-                var result = GetCode(string.Format(urlTemplate, startId + offset)).Result;
-                switch (result)
-                {
-                    case HttpStatusCode.NotFound:
-                        notFountCount++;
-                        break;
-                    case HttpStatusCode.OK:
-                        okCount++;
-                        break;
-                    default:
-                        Console.WriteLine(result.ToString());
-                        break;
-                }
-                offset--;
-                Console.WriteLine(string.Format("OK: {0}, NotFound: {1}", okCount, notFountCount));
+                throw new ArgumentException("The radix must be >= 2 and <= " +
+                    Digits.Length.ToString());
             }
-        }
-
-        public static async Task ListSearch()
-        {
-            var client = new HttpClient();
-            var responseJson = await client.GetStringAsync("https://ssl.olx.ua/i2/ads/?json=1&longlist=1&search");
-
+            if (String.IsNullOrEmpty(number))
+            {
+                throw new Exception();
+            }
+            long result = 0;
+            long multiplier = 1;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char c = number[i];
+                if (i == 0 && c == '-')
+                {
+                    result = -result;
+                    break;
+                }
+                int digit = Digits.IndexOf(c);
+                if (digit == -1)
+                    throw new ArgumentException(
+                        "Invalid character in the arbitrary numeral system number",
+                        "number");
+                result += digit * multiplier;
+                multiplier *= radix;
+            }
+            return result;
         }
     }
 }
--- src/OlxUa/Properties/AssemblyInfo.cs
+++ src/OlxUa/Properties/AssemblyInfo.cs
@@ -5,23 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("OlxUa")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("$safeprojectname$")]
-[assembly: AssemblyCopyright("Copyright ©  116")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("C252E7FD-D012-4E16-8210-5E4A078CAE40")]
-
+[assembly: Guid("a2bfffdd-3101-445f-bdca-f4bd99351b45")]
