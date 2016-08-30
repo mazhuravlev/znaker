f1cc64881379020817bd54fae47cbd5b1a51d4cb
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-08-30 12:11:00 +0300

    olx id decrypt test

---
+++ src/OlxLib/Utils/IdUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace OlxLib
+{
+    public class IdUtils
+    {
+        public static long DecryptOlxId(string olxId)
+        {
+            return ArbitraryToDecimalSystem(SwapCase(olxId), 62);
+        }
+        private static string SwapCase(string input)
+        {
+            return new string(input.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ?
+                       char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+        }
+        private static long ArbitraryToDecimalSystem(string number, int radix)
+        {
+            const string Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            if (radix < 2 || radix > Digits.Length)
+            {
+                throw new ArgumentException("The radix must be >= 2 and <= " +
+                    Digits.Length.ToString());
+            }
+            if (String.IsNullOrEmpty(number))
+            {
+                throw new Exception();
+            }
+            long result = 0;
+            long multiplier = 1;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char c = number[i];
+                if (i == 0 && c == '-')
+                {
+                    result = -result;
+                    break;
+                }
+                int digit = Digits.IndexOf(c);
+                if (digit == -1)
+                    throw new ArgumentException(
+                        "Invalid character in the arbitrary numeral system number",
+                        "number");
+                result += digit * multiplier;
+                multiplier *= radix;
+            }
+            return result;
+        }
+    }
+}
---
+++ test/Parsers/Olx/OlxLib.Test/Tests.cs
@@ -0,0 +1,20 @@
+﻿using OlxLib;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestRightResult() 
+        {
+            Assert.Equal(IdUtils.DecryptOlxId("fzaOY"), 230028120);
+        }
+
+        [Fact]
+        public void TestWrongResult()
+        {
+            Assert.NotEqual(IdUtils.DecryptOlxId("fzaOY"), 1);
+        }
+    }
+}
