02ceca46ed526002c0682630da2756de0d591392
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-09-03 01:49:03 +0300

    DownloadWorker

---
+++ src/OlxLib/Utils/TextUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OlxLib.Utils
+{
+    public class TextUtils
+    {
+        public static string CleanSpacesAndNewlines(string s)
+        {
+            return Regex.Replace(s, @"[\t\n\r]+", " ");
+        }
+    }
+}
--- src/OlxLib/Workers/DownloadWorker.cs
+++ src/OlxLib/Workers/DownloadWorker.cs
@@ -1,18 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using Hangfire.Server;
-using Hangfire.Storage.Monitoring;
+using Infrastructure;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using OlxLib.Entities;
+using OlxLib.Utils;
 
 namespace OlxLib.Workers
 {
     public class DownloadWorker
     {
-        private ParserContext _db;
+        private readonly ParserContext _db;
         private OlxConfig _config;
 
         public DownloadWorker(ParserContext db)
@@ -32,7 +32,7 @@ namespace OlxLib.Workers
 
         private DownloadJob GetNextJob()
         {
-            return _db.DownloadJobs.First(dj => dj.OlxResponse != OlxResponse.Ok);
+            return _db.DownloadJobs.First(dj => dj.ProcessedAt == null);
         }
 
         private void ProcessJob(DownloadJob downloadJob)
@@ -43,18 +43,94 @@ namespace OlxLib.Workers
             var contactsResponseTask = client.GetAsync(_config.GetAdvertContactUrl(downloadJob.AdvId));
             downloadJob.AdHttpStatusCode = adResponse.StatusCode;
             if (!adResponse.IsSuccessStatusCode) return;
+            var adText = ExtractAdTextFromJsonString(adResponse.Content.ReadAsStringAsync().Result);
             var exportJob = new ExportJob
             {
                 DownloadJob = downloadJob,
                 Data = new OlxAdvert
                 {
-                    Text = (string)JsonConvert.DeserializeObject<JObject>(adResponse.Content.ReadAsStringAsync().Result)["ad"]["description"]
+                    Text = adText
                 }
             };
             _db.Add(exportJob);
             var contactsResponse = contactsResponseTask.Result;
             downloadJob.ContactsHttpStatusCode = contactsResponse.StatusCode;
+            if (!contactsResponse.IsSuccessStatusCode) return;
+            var contacts = ExtractAdContactsFromJsonString(contactsResponse.Content.ReadAsStringAsync().Result);
+            if (null == contacts) return;
+            exportJob.Data.Contacts = contacts;
             downloadJob.ProcessedAt = DateTime.Now;
+        }
+
+        private static List<KeyValuePair<ContactType, string>> ExtractAdContactsFromJsonString(string contactsJsonString)
+        {
+            JObject jObject;
+            try
+            {
+                jObject = JsonConvert.DeserializeObject<JObject>(contactsJsonString);
+            }
+            catch (JsonReaderException)
+            {
+                // invalid json
+                return null;
+            }
+            JToken phoneJToken;
+            try
+            {
+                phoneJToken = jObject["urls"]["phone"];
+            }
+            catch (NullReferenceException)
+            {
+                // json doesn't have required fields
+                return null;
+            }
+            List<string> phoneStringList;
+            switch (phoneJToken.Type)
+            {
+                case JTokenType.Array:
+                    phoneStringList = ExtractPhonesFromJson(phoneJToken.Children());
+                    break;
+                case JTokenType.Object:
+                    phoneStringList = ExtractPhonesFromJson(new List<JToken> {phoneJToken});
+                    break;
+                default:
+                    throw new Exception("Unknown type: " + jObject.Type);
+            }
+            return phoneStringList
+                .Select(s => new KeyValuePair<ContactType, string>(ContactType.Phone, s))
+                .ToList();
+        }
+
+        private static List<string> ExtractPhonesFromJson(IEnumerable<JToken> phoneJtokenList)
+        {
+            return phoneJtokenList
+                .Select(jt => PhoneNormalizer.Normalize((string) jt["uri"]))
+                .ToList();
+        }
+
+        private static string ExtractAdTextFromJsonString(string adJsonString)
+        {
+            JObject jObject;
+            try
+            {
+                jObject = JsonConvert.DeserializeObject<JObject>(adJsonString);
+            }
+            catch (JsonReaderException)
+            {
+                // invalid json
+                return null;
+            }
+            string text;
+            try
+            {
+                text = (string) jObject["ad"]["title"] + ", " + (string) jObject["ad"]["description"];
+            }
+            catch (NullReferenceException)
+            {
+                // json doesn't have required fields
+                return null;
+            }
+            return TextUtils.CleanSpacesAndNewlines(text);
         }
     }
 }
