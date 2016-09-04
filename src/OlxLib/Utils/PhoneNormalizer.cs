b79c6343968a34f8b2236436296144734302e6ff
longday <6600gm@gmail.com>
2016-09-05 02:59:39 +0300

    PhoneNormalizer add missing rules

--- src/OlxLib/Utils/PhoneNormalizer.cs
+++ src/OlxLib/Utils/PhoneNormalizer.cs
@@ -9,13 +9,43 @@ namespace OlxLib.Utils
 {
     public class PhoneNormalizer
     {
-        private static readonly Regex GoodNumber = new Regex(@"(7|38)\d{10}");
-        private static readonly Regex UaMobile = new Regex(@"(38)?(?<base>0(39|50|63|66|67|68|91|92|93|94|95|96|97|98|99)\d{7})");
-        private static readonly Regex RuMobile = new Regex(@"(8|7)?(?<base>9(00|01|02|03|04|05|06|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|36|37|38|39|50|51|52|53|58|60|61|62|63|64|65|66|67|68|69|77|78|80|81|82|83|84|85|86|87|88|89|91|92|93|94|95|96|99)\d{7})");
-
+        //                                                      Ru/Kz-Ua          By-Uz
+        private static readonly Regex GoodNumber = new Regex(@"^((7|38)\d{10})|((375|998)\d{9})$");
         private static readonly Regex CleanRegex = new Regex(@"\D");
+        private static readonly List<CountryRule> Rules = new List<CountryRule>
+        {
+            new CountryRule
+            {
+                Country = "Ua",
+                Prefix = "38",
+                Rule = new Regex(@"^(38)?(?<base>0(39|50|63|66|67|68|91|92|93|94|95|96|97|98|99)\d{7})$"),
+                Weight = 1
+            },
+            new CountryRule
+            {
+                Country = "By",
+                Prefix = "375",
+                Rule = new Regex(@"^(375|80)(?<base>(24|25|29|33|44)\d{7})$"),
+                Weight = 2
+            },
+            new CountryRule
+            {
+                Country = "Ru",
+                Prefix = "7",
+                Rule = new Regex(@"^(8|7)?(?<base>9(00|01|02|03|04|05|06|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|36|37|38|39|50|51|52|53|58|60|61|62|63|64|65|66|67|68|69|77|78|80|81|82|83|84|85|86|87|88|89|91|92|93|94|95|96|99)\d{7})$"),
+                Weight = 2
+            },
+            new CountryRule
+            {
+                Country = "Kz",
+                Prefix = "7",
+                Rule = new Regex(@"^(8|7)?(?<base>7(00|01|02|05|07|27|47|71|75|76|77|78)\d{7})$"),
+                Weight = 2
+            }
+        };
 
 
+        
         public static string Normalize(string phone)
         {
             if (!phone.HasText())
@@ -28,25 +58,33 @@ namespace OlxLib.Utils
             {
                 throw new FormatException("Ну и что с такими делать, будем добавлять?");
             }
+
             if (GoodNumber.IsMatch(phone))
             {
                 return phone;
             }
 
-            var uaMatch = UaMobile.Match(phone);
-            if (uaMatch.Success)
-            {
-                return $"38{uaMatch.Groups["base"].Value}";
-            }
 
-            var ruMatch = RuMobile.Match(phone);
-            if (ruMatch.Success)
+            foreach (var rule in Rules.OrderBy(c => c.Weight))
             {
-                return $"7{ruMatch.Groups["base"].Value}";
+                var match = rule.Rule.Match(phone);
+                if (match.Success)
+                {
+                    return rule.Prefix + match.Groups["base"].Value;
+                }
             }
-            
             throw new FormatException("Что то непонятное тут, что с такими делать, будем добавлять?");
         }
-
+        protected class CountryRule
+        {
+            public string Prefix;
+            public string Country;
+            public Regex Rule;
+            public int Weight;
+            public OlxType? OlxType;
+        }
     }
+
+    
+
 }
--- test/OlxLib.Test/PhoneNormalizerTests.cs
+++ test/OlxLib.Test/PhoneNormalizerTests.cs
@@ -10,9 +10,10 @@ namespace OlxLib.Test
 {
     public class PhoneNormalizerTests
     {
+        //http://simcard.kz/
 
         [Fact]
-        public void TestNonPhones()
+        public void TestPhoneNormalizer()
         {
             Assert.Throws<ArgumentException>(() => PhoneNormalizer.Normalize(""));
             Assert.Throws<ArgumentException>(() => PhoneNormalizer.Normalize(null));
@@ -21,32 +22,39 @@ namespace OlxLib.Test
             Assert.Throws<FormatException>(() => PhoneNormalizer.Normalize("12345"));
             Assert.Throws<FormatException>(() => PhoneNormalizer.Normalize("12345 12345 12345"));
             Assert.Throws<FormatException>(() => PhoneNormalizer.Normalize("11118543065"));
-        }
+            Assert.Throws<FormatException>(() => PhoneNormalizer.Normalize("8 067 856 4569")); // old ua format
 
 
-        [Fact]
-        public void TestRuMobile()
-        {
-            var actual = "79788543065";
+            var actual = "79788543065"; //ru
             Assert.Equal(PhoneNormalizer.Normalize("+79788543065"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("+7(978)8543065"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("+7 978) 854 30 65"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("89788543065"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("suka:89788543065"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("978-854-30-65"), actual);
-        }
 
-        [Fact]
-        public void TestUaMobile()
-        {
-            var actual = "380678564569";
+            actual = "380678564569"; // ua
             Assert.Equal(PhoneNormalizer.Normalize("+380678564569"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("+38 067 856 4569"), actual);
-            Assert.Equal(PhoneNormalizer.Normalize("8 067 856 4569"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize(" 067 856 4569"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("0678564569"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("06785-64569"), actual);
             Assert.Equal(PhoneNormalizer.Normalize("pidor:0678564569"), actual);
-        }
 
+            actual = "375295830468"; //by
+            Assert.Equal(PhoneNormalizer.Normalize("+375295830468"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("+3752 95830 468"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("80295830468"), actual);
+
+            actual = "77027007077"; //kz
+            Assert.Equal(PhoneNormalizer.Normalize("+77027007077"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("+77 02 7007 077"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("87027007077"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("7027007077"), actual);
+
+            actual = "998994055804"; //uz
+            Assert.Equal(PhoneNormalizer.Normalize("+998994055804"), actual);
+            Assert.Equal(PhoneNormalizer.Normalize("99 89 94055 804"), actual);
+        }
     }
 }
