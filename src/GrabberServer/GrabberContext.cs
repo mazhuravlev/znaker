e55034540ca775170d37f2ae3de348f0ea353006
longday <6600gm@gmail.com>
2016-09-07 15:12:37 +0300

    grabber server add context

---
+++ src/GrabberServer/GrabberContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrabberServer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrabberServer
+{
+    public class GrabberContext : DbContext
+    {
+        public DbSet<SitemapEntry> SitemapEntries { get; set; }
+        public GrabberContext(DbContextOptions<GrabberContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            b.Entity<SitemapEntry>(
+                //c => c
+            );
+        }
+    }
+}
--- src/GrabberServer/Startup.cs
+++ src/GrabberServer/Startup.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.Parser;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PostgreSqlProvider;
+using Microsoft.EntityFrameworkCore;
 
 namespace GrabberServer
 {
@@ -27,6 +30,10 @@ namespace GrabberServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GrabberContext>(c => c.UseNpgsql(Configuration["ConnectionStrings:GrabberConnectionString"]), ServiceLifetime.Transient);
+            services.AddDbContext<ZnakerContext>(c => c.UseNpgsql(Configuration["ConnectionStrings:ZnakerConnectionString"]), ServiceLifetime.Transient);
+
+
             // Add framework services.
             services.AddMvc();
         }
