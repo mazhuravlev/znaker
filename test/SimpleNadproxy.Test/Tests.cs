69fc50e1c73e2d193712615c2135f87ca16148af
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-09-08 08:31:02 +0300

    updated SimpleNadproxy.Test

--- test/SimpleNadproxy.Test/Tests.cs
+++ test/SimpleNadproxy.Test/Tests.cs
@@ -1,35 +1,41 @@
 ﻿using System;
-using System.Net;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Runtime.InteropServices.ComTypes;
-using System.Threading.Tasks;
-using GrabberServer.Grabbers.Nadproxy;
 using NuGet.Configuration;
 using Xunit;
 
-namespace Nadproxy.Test
+namespace SimpleNadproxy.Test
 {
     public class Tests
     {
+        private const string GoodProxyUrl = "http://localhost:8888";
+        private const string BadProxyUrl = "http://unreachable";
+        private const string GoodUrl = "http://example.com";
+        private const string BadUrl = "http://unreachable";
+
         [Fact]
         public void TestSuccess()
         {
-            var nadproxy = new SimpleNadproxy(new WebProxy("http://localhost:8888"));
-            var response = nadproxy.GetAsync("http://example.com/").Result;
+            var nadproxy = new GrabberServer.Grabbers.Nadproxy.SimpleNadproxy(new WebProxy(GoodProxyUrl));
+            var response = nadproxy.GetAsync(GoodUrl).Result;
             Assert.IsAssignableFrom<HttpResponseMessage>(response);
             Assert.True(response.IsSuccessStatusCode);
             Assert.Contains("Example Domain", response.Content.ReadAsStringAsync().Result);
         }
 
         [Fact]
-        public void TestFail()
+        public void TestBadUrl()
         {
-            var nadproxy = new SimpleNadproxy(new WebProxy("http://localhost:8888"));
-            var response = nadproxy.GetAsync("http://unreachable/").Result;
+            var nadproxy = new GrabberServer.Grabbers.Nadproxy.SimpleNadproxy(new WebProxy(GoodProxyUrl));
+            var response = nadproxy.GetAsync(BadUrl).Result;
             Assert.IsAssignableFrom<HttpResponseMessage>(response);
             Assert.True(!response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public void TestBadProxyFail()
+        {
+            var nadproxy = new GrabberServer.Grabbers.Nadproxy.SimpleNadproxy(new WebProxy(BadProxyUrl));
+            Assert.ThrowsAny<Exception>(() => nadproxy.GetAsync(GoodUrl).Result);
         }
     }
 }
