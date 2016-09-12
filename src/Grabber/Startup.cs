dc78977b730a88dc139437af3fcbadaad695bfcf
longday <6600gm@gmail.com>
2016-09-12 14:25:39 +0300

    fix Configure args

--- src/Grabber/Startup.cs
+++ src/Grabber/Startup.cs
@@ -28,7 +28,7 @@ namespace Grabber
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider provider, , IApplicationLifetime appLifetime)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider provider, IApplicationLifetime appLifetime)
         {
             loggerFactory.AddConsole();
 
