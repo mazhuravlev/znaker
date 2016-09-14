bdd43cc9f600fe61189a309e59007689e1ee4e5f
Mikhail Zhuravlev <zhuravlev@etton.ru>
2016-09-14 17:34:34 +0300

    added ProxyService

---
+++ src/Grabber/Infrastructure/Http/SimpleNadproxy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Grabber.Infrastructure.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Grabber.Infrastructure.Http
+{
+    public class SimpleNadproxy : IHttpClient
+    {
+        private readonly IProxyService _proxyService;
+        private readonly List<HttpClient> _httpClients = new List<HttpClient>();
+        private int _counter = 0;
+        private const int ProxyLimit = 10;
+
+        public SimpleNadproxy(IProxyService proxyService)
+        {
+            _proxyService = proxyService;
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {
+            if (_httpClients.Count == 0)
+            {
+                return Task.Factory.StartNew(() => new HttpResponseMessage(HttpStatusCode.ServiceUnavailable));
+            }
+            if (_httpClients.Count < ProxyLimit)
+            {
+                RequestProxy();
+            }
+            return _httpClients[_counter++ % _httpClients.Count].GetAsync(url);
+        }
+
+        private void RequestProxy()
+        {
+            try
+            {
+                var proxy = _proxyService.GetProxy();
+                // TODO: somehow use the proxy
+            }
+            catch (NoResultException)
+            {
+                // no proxies available in list
+            }
+        }
+    }
+}
---
+++ src/Grabber/Infrastructure/Services/ProxyService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Grabber.Infrastructure.Services
+{
+    public class NoResultException : Exception
+    {
+    }
+
+    public class Proxy
+    {
+        public string Host;
+        public int Port;
+    }
+
+    public interface IProxyService
+    {
+        Proxy GetProxy();
+    }
+
+    public class ProxyService
+    {
+        private readonly IModel _channel;
+        private const string QueueName = "proxy_list";
+
+        public ProxyService()
+        {
+            _channel = new ConnectionFactory {HostName = "localhost"}.CreateConnection().CreateModel();
+            _channel.QueueDeclare(
+                queue: QueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        public Proxy GetProxy()
+        {
+            var proxyListEntry = _channel.BasicGet(QueueName, true);
+            if (proxyListEntry == null) throw new NoResultException();
+            return JsonConvert.DeserializeObject<Proxy>(Encoding.UTF8.GetString(proxyListEntry.Body));
+        }
+    }
